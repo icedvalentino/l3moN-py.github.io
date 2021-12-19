504d066a99e6b8bdd48810d59ef61690c6be14c1
Klacik <l3mon.off@gmail.com>
2021-12-19 16:45:45 +0100

    Co-authored-by: icedvalentino <icedvalentino@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,12 @@ function clear_var() {
     i = 0;
     console.log(`"${i}"`);
 }
+
+function change_map(mapName) {
+    // return `"/images/csgo_maps/${mapName}.png)"`
+    console.log('TT')
+    document.getElementById("mapImage").style.backgroundColor = "white";
+    // document.getElementById("mapImage").style.content = "/images/csgo_maps/inferno.png)";
+    // document.getElementById("mapImage").style.content = url(`"/images/csgo_maps/${mapName}.png)"`);
+}
+// console.log(change_map("nuke"));
