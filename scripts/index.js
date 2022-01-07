15070336fb9534f4fc0aea2ab9529095e701634d
Klacik <l3mon.off@gmail.com>
2022-01-07 18:14:39 +0100

    footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,6 @@
 console.log('JS working');
 var i = 0;
 
-function count_var() {
-    i += 1;
-    console.log(`"${i}"`);
+function open_in_tab() {
+    window.open('https://github.com/l3moN-py', '_blank');
 }
-
-function clear_var() {
-    i = 0;
-    console.log(`"${i}"`);
-}
-
-function change_map(mapName) {
-    // return `"/images/csgo_maps/${mapName}.png)"`
-    console.log('TT')
-    document.getElementById("mapImage").style.backgroundColor = "white";
-    // document.getElementById("mapImage").style.content = "/images/csgo_maps/inferno.png)";
-    // document.getElementById("mapImage").style.content = url(`"/images/csgo_maps/${mapName}.png)"`);
-}
-// console.log(change_map("nuke"));
